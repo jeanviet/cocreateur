981a2cde869be94971fce69266c2a2a6fd03e6cc
jeanviet <53624820+jeanviet@users.noreply.github.com>
2024-07-27 15:44:59 +0200

    Update split3audio.py

--- scripts/split3audio.py
+++ scripts/split3audio.py
@@ -1,6 +1,11 @@
 from pydub import AudioSegment
+import os
 
-def split_mp3_into_three(input_file):
+def split_mp3_into_three(input_file, output_folder):
+    # Créer le répertoire de sortie s'il n'existe pas
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
     # Charger le fichier audio
     audio = AudioSegment.from_mp3(input_file)
     
@@ -15,10 +20,13 @@ def split_mp3_into_three(input_file):
     ]
     
     # Exporter les segments
+    base_filename = os.path.splitext(os.path.basename(input_file))[0]
     for i, segment in enumerate(segments, start=1):
-        segment.export(f"{input_file[:-4]}_part{i}.mp3", format="mp3")
-        print(f"Segment {i} exporté : {input_file[:-4]}_part{i}.mp3")
+        output_file = os.path.join(output_folder, f"{base_filename}_part{i}.mp3")
+        segment.export(output_file, format="mp3")
+        print(f"Segment {i} exporté : {output_file}")
 
 # Exemple d'utilisation
 input_file = 'audio.mp3'
-split_mp3_into_three(input_file)
+output_folder = 'split3'
+split_mp3_into_three(input_file, output_folder)
